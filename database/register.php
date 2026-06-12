633b2acf580b3a3d83cd5f789f2de9abc4fa710f
Vyncenzo <jvbinas055@gmail.com>
2026-06-13 07:18:19 +0800

    git all .

--- database/register.php
+++ database/register.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: https://github.io");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 $host     = 'mysql.railway.internal';
 $db       = 'YOUR_DATABASE_NAME';
 $user     = 'root';
--- scripts/logout.js
+++ scripts/logout.js
@@ -1,10 +1,3 @@
-<?php
-
-header("Access-Control-Allow-Origin: https://github.io");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
